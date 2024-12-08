6347bfacb3313c25455c93ff090ff08c16fd2ffb
shaker <hydrashaker@gmail.com>
2024-12-08 12:40:28 +0530

    overview updated

---
+++ Typescript/type.ts
@@ -0,0 +1,18 @@
+/*
+->introduction to TS
+  - TypeScript is a superset of JavaScript
+  - statically typed
+->Anders Hejlsberg
+  -created TypeScript in 2010 at Microsoft
+  -"But how it works ?"
+   -TypeScript Source Code(.ts file) -->  JavaScript code .js File --> V8 JavaScript Engine
+
+ ->Why TypeScript
+  -large-scale Applications
+  -Error Detection
+  -Future-proofing 
+  -self-Documenting code
+  -code Quality
+  -Faster Debbuging
+   
+*/
